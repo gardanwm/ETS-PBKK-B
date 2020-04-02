e0b9eb282b10b5ef5a65037eaa868c2217bc43d5
Garda Negara W <45958288+gardanwm@users.noreply.github.com>
2020-04-02 11:09:21 +0700

    ETS-PBKK-B

---
+++ POS/application/views/v_dashboard.php
@@ -0,0 +1,34 @@
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="page-header">
+                            Dashboard
+                        </h2>
+                    </div>
+                </div>
+
+
+                <div class="row">
+
+
+                    <div class="col-md-9 col-sm-12 col-xs-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                Bar Chart Example
+                            </div>
+                            <div class="panel-body">
+                                <div id="morris-bar-chart"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-12 col-xs-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                Donut Chart Example
+                            </div>
+                            <div class="panel-body">
+                                <div id="morris-donut-chart"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
